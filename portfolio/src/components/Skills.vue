a9b1a6fea9791618c89602ca056d9f73dfef3cd2
katebennu <kate.dorrer@gmail.com>
2017-10-14 19:06:07 +0300

    basic conditional filtering

--- portfolio/src/components/Skills.vue
+++ portfolio/src/components/Skills.vue
@@ -1,9 +1,39 @@
 <template>
-  <div><h1>Skills</h1></div>
+  <div>
+    <h1>Skills</h1>
+    <ul>
+      <li v-for="skill in skillList"
+          v-if="areachoice.includes(skill.type) &&
+          skill.level >= levelchoice">{{skill.name}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'skills',
+    data() {
+      return {
+        skillList: [
+          {
+            name: 'Python',
+            type: 'tech',
+            level: 5,
+          },
+          {
+            name: 'JavaScript',
+            type: 'tech',
+            level: 6,
+          },
+          {
+            name: 'Business Development',
+            type: 'biz',
+            level: 4,
+          },
+        ],
+        areachoice: ['tech'],
+        levelchoice: 6,
+      };
+    },
   };
 </script>
